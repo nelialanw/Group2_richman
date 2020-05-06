ae07ec2f9a4f29571ca15a591b3a4de25bd89829
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-07 01:24:44 +0800

    Update and rename save_richman.h to richMan_save.h

---
+++ richMan_save.h
@@ -0,0 +1,7 @@
+//richMan_save.h
+#ifndef RICHMAN_SAVE_H
+#define RICHMAN_SAVE_H
+
+void richMan_save(std::string player_status);
+
+#endif
--- save_richman.h
+++
@@ -1,7 +0,0 @@
-//save_richman.h
-#ifndef SAVE_RICHMAN_H
-#define SAVE_RICHMAN_H
-
-void save_richman(std::string player_status);
-
-#endif
