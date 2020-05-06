353dac8d5605cc5209722cfe84b562a34c435625
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-06 19:02:00 +0800

    Add files via upload

--- load_richman.cpp
+++ load_richman.cpp
@@ -1,29 +1,46 @@
 // load_richman.cpp
 #include <iostream>
-#include <boost/filesystem.hpp>
+#include <fstream>
 #include "load_richman.h"
-using namespace std;
-using namespace boost::filesystem;
+#include <cstdlib>
+#include <string>
+#include <sys/stat.h>
+#include <dirent.h>
 
-void load_richman()
+using namespace std;
+
+void load_richman(char *save){
   string slot_name;
-  path p("/save/");
-  int j = 1;
-  for (auto i = directory_iterator(p); i != directory_iterator(); i++)
-  {
-    if (!is_directory(i->path()))
-    {
-      cout << i->path().filename.string() << endl;
-      j++;
-    }
-    else
-      continue;
+  //const char save[10] = "save";
+  DIR *dir= opendir(save);
+
+  struct dirent *entry;
+  struct stat info;
+
+  if (!dir) {
+    cout << "Directory was not found\n" << endl;
+    return;
   }
+  entry = readdir(dir);
+//(entry = readdir(dir)) != NULL
+  while ((entry = readdir(dir)) != NULL ){
+    if ( entry->d_name[0] != '.'){
+      string name = string(entry->d_name);
+      name = name.erase(name.find(".txt"), 4);
+      cout << name << endl;
+      stat(name.c_str(), &info);
+          /*if (S_ISDIR(info.st_mode)) {
+            load_richman(save);
+          }*/
+    }
+  }
+  closedir(dir);
+
   cout << "Which slot do you want to load? (Name of file)";
   cin >> slot_name;
   string stringpath = "save";
   int status = mkdir(stringpath.c_str(),0777);
-  ifstream fin("save/"+slot_name); //change the cmd when the name of directory changed
+  ifstream fin("save/"+slot_name+".txt"); //change the cmd when the name of directory changed
   if (fin.fail()) {
     //the error message to notify failed opening; (1) for importing text
     cout << "Error in the file opening!" << endl;
@@ -33,4 +50,10 @@ void load_richman()
     //use linked_list, depends on main()'s format
   }
   fin.close();
+}
+
+int main(){
+  char save[10] = "save";
+  load_richman(save);
+  return 0;
 }
