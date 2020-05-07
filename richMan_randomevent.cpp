f4a403064ec824b942211d4e60a3819804d0a508
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-07 21:58:37 +0800

    Update and rename random_event.cpp to richMan_randomevent.cpp

---
+++ richMan_randomevent.cpp
@@ -0,0 +1,53 @@
+//richMan_randomevent.cpp
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <sys/stat.h>
+#include "richMan_struct.h"
+#include "richMan_load.h"
+
+
+using namespace std;
+
+void random_event(string event, struct Status *players, int player_code) 
+{
+  string result;
+  int random_number, i=0;
+  srand(time(NULL));
+  random_number = rand() %10;
+  ifstream fin("event/" + event + ".txt");
+  if (fin.fail()) {
+    //the error message to notify failed opening; (1) for importing text
+    cout << "Error in the file opening!(1)" << endl;
+    exit(1);
+  }
+  int j = 0;
+  while (j != random_number)
+  {
+    string temp;
+    getline(fin, temp);
+    getline(fin, temp);
+    j++;
+  }
+  if (j == random_number){
+    string temp;
+    int read_num;
+    fin >> temp;
+    fin >> read_num;
+    if (temp == "a") {
+      for (int i = 0; i<4 ;i++){
+        players[i].cash += read_num;
+      }
+    }
+    else {
+      players[player_code].cash += read_num;
+    }
+    fin >> read_num;
+    players[player_code].position += read_num;
+    getline(fin,temp);
+    cout << temp << endl;
+  }
+  fin.close();
+}
