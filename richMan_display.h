3856b84b9c0358009b89ea866d3bbed298f22b82
WongWingLam <u3568502@connect.hku.hk>
2020-05-07 14:49:26 +0800

    Update richMan_display.h

--- richMan_display.h
+++ richMan_display.h
@@ -1,7 +1,8 @@
 //richMan_display.h
 #ifndef RICHMAN_DISPLAY_H
 #define RICHMAN_DISPLAY_H
+#include "richMan_struct.h"
 
-void displayMap(Block *mapBlocks);
+void displayMap(Block *mapBlocks, Status *player, int playerNo);
 
 #endif
