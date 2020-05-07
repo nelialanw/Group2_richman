cbbe0ab1cbed9e7e591b59edf9b98ad23be419b0
WongWingLam <u3568502@connect.hku.hk>
2020-05-07 17:32:59 +0800

    Add files via upload

---
+++ richMan_mapEffect.h
@@ -0,0 +1,7 @@
+#ifndef RICHMAN_MAPEFFECT_H
+#define RICHMAN_MAPEFFECT_H
+#include "richMan_struct.h"
+
+void mapEffect(int dice, int turn, Block *mapBoard, Status *players, int playerNo);
+
+#endif
