522fa5d06d5f52dddf1ccbe66ad93cba6720a591
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-07 22:04:05 +0800

    Update and rename random_event.h to richMan_randomevent.h

--- random_event.h
+++
@@ -1,7 +0,0 @@
-//random_event.h
-#ifndef RANDOM_EVENT_H
-#define RANDOM_EVENT_H
-
-string random_event(string event);
-
-#endif
---
+++ richMan_randomevent.h
@@ -0,0 +1,8 @@
+//richMan_randomevent.h
+#ifndef RICHMAN_RANDOMEVENT_H
+#define RICHMAN_RANDOMEVENT_H
+#include "richMan_struct.h"
+
+void random_event(string event, struct Status *players, int player_code) ;
+
+#endif
