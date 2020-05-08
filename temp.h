037428d6836979a980572bec56ee75576a897d71
WongWingLam <u3568502@connect.hku.hk>
2020-05-08 20:18:37 +0800

    Update temp.h

--- temp.h
+++ temp.h
@@ -1,6 +1,6 @@
 //richMan_load.h
-#ifndef RICHMAN_LOAD_H
-#define RICHMAN_LOAD_H
+#ifndef TEMP_H
+#define TEMP_H
 #include "richMan_struct.h"
 
 void save_temp(Block *mapBlocks, Status *players, int n, int round, int turn, int mode);
