ff2b205f60f455cda3b67cd46de92e60a5307fb0
WongWingLam <u3568502@connect.hku.hk>
2020-05-08 23:20:43 +0800

    Add files via upload

---
+++ richMan_rule.cpp
@@ -0,0 +1,27 @@
+// richMan_rule.cpp
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+void rule()
+{
+  ifstream fin("readme.txt"); //change the cmd when the name of directory changed
+  if (fin.fail())
+  {
+    //the error message to notify failed opening; (1) for importing text
+    cout << "Error in the file opening!" << endl;
+    exit(1);
+  }
+
+  string line;
+  while (getline(fin, line))
+  {
+    cout << line << end;
+  }
+  fin.close();
+
+  return;
+}
