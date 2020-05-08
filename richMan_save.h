3424ffafaa3199d93f073bb2919f61dac3b5a9b2
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-08 18:17:03 +0800

    Update richMan_save.h

--- richMan_save.h
+++ richMan_save.h
@@ -3,6 +3,6 @@
 #define RICHMAN_SAVE_H
 #include "richMan_struct.h"
 
-void richMan_save(Block *mapBlocks, Status *players, int n, int round, int turn);
+void richMan_save(Block *mapBlocks, Status *players, int n, int round, int turn, int mode);
 
 #endif
