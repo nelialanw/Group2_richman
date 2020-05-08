4abd470a8de3a2ab95460d0e4575c76dad313a13
Square Alan Li <63199590+nelialanw@users.noreply.github.com>
2020-05-08 16:48:21 +0800

    Update richMan_mapEffect.cpp

--- richMan_mapEffect.cpp
+++ richMan_mapEffect.cpp
@@ -12,7 +12,7 @@ using namespace std;
 void mapEffect(int dice, int turn, Block *mapBoard, Status *players, int playerNo, int *freeze){
   int choice = 1;
   int pos = players[turn].position;
-  while (choice)
+  while (choice && (mode ==1 || (mode == 2 && turn ==0)))
   {
     //system("CLS");
     displayMap(mapBoard, players, playerNo);
@@ -78,5 +78,57 @@ void mapEffect(int dice, int turn, Block *mapBoard, Status *players, int playerN
     cout << endl << "Enter 0 to continue the game: ";
     cin >> choice;
   }
+  
+  while (choice && mode == 2 && turn !=0)
+  {
+    if (players[turn].cash > mapBoard[pos].price && mapBoard[pos].ownership == -1)
+    {
+      int buy;
+      cout << "Buy " << mapBoard[pos].name << "(price: "<< mapBoard[pos].price << ')' << " ? (1:buy 0:not buy)";
+      srand(time(NULL));
+      buy = rand()%2;
+      if (buy)
+      {
+        mapBoard[pos].ownership = turn;
+        players[turn].cash -= mapBoard[pos].price;
+        cout << "## " << players[mapBoard[pos].ownership].name << " become the owner of " << mapBoard[pos].name << " ##" << endl
+        << "## Cash remaining: " << players[turn].cash << " ##" << endl;
+        players[turn].property += 1;
+      }
+    }
+    else if (mapBoard[pos].ownership == turn && players[turn].cash >= mapBoard[pos].price*0.1)
+    {
+      int invest = 1;
+      if(invest)
+      {
+        mapBoard[pos].Lv += 1;
+        players[turn].cash -= mapBoard[pos].price*0.1;
+        mapBoard[pos].price *= 1.1;
+      }
+    }
+    else if(mapBoard[pos].ownership >= 0 && mapBoard[pos].ownership < playerNo && mapBoard[pos].ownership != turn)
+    {
+      players[turn].cash -= mapBoard[pos].price*0.1;
+      players[mapBoard[pos].ownership].cash += mapBoard[pos].price*0.1;
+    }
+    else if (mapBoard[pos].name.compare("Chance") == 0)
+    {
+      random_event("chance", players, turn);
+    }
+    else if (mapBoard[pos].name.compare("Destiny") == 0)
+    {
+      random_event("destiny", players, turn);
+      if (players[turn].position == 9)
+      {
+        freeze[turn] = 3;
+      }
+      if (players[turn].position == 18)
+      {
+        freeze[turn] = 2;
+      }
+    }
+    choice = 0;
+  }
+  
   return;
 }
